37ca557b984ef68efc01e081126a5d234ab9aeb0
Al Cerdan Lico <alcerdan@yahoo.com>
2024-11-09 23:39:02 -0800

    add navlink to navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,16 @@
+import Search from './Search'
+import { NavLink } from 'react-router-dom'
+import './App.css';
+
 function NavBar() {
+
     return (
         <div>
-            <h1>StockY</h1>
+            <h1 className='h1header'>StockY</h1>
+            <nav>
+                <
+            </nav>
+            <Search/>
         </div>
     )
 }
---
+++ src/Search.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Search() {
+    return (
+        <div className='searchbar'>
+            <input type='text' placeholder='Search for shoes'></input>
+        </div>
+    )
+}
+
+export default Search
