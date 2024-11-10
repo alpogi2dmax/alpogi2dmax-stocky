3d9afc182bbd89eea9676e4f937975097eb17366
Al Cerdan Lico <alcerdan@yahoo.com>
2024-11-09 23:14:41 -0800

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar'
 
 function App() {
   return (
     <div >
-      <header >
-        
+      <header className='App-newheader'>
+        <NavBar />
       </header>
     </div>
   );
---
+++ src/NavBar.js
@@ -0,0 +1,9 @@
+function NavBar() {
+    return (
+        <div>
+            <h1>StockY</h1>
+        </div>
+    )
+}
+
+export default NavBar
